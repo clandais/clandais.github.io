824cfce558d3a3092cb78c05407faa6645b2c6ed
Clément <clementlandais@gmail.com>
2025-04-15 17:42:04 +0200

    Fixed "edited" property missing in "project"  collection definintion

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,6 +31,7 @@ const projects = defineCollection({
     demoURL: z.string().optional(),
     repoURL: z.string().optional(),
     websiteURL: z.string().optional(),
+    edited: z.coerce.date().optional(),
   }),
 });
 
